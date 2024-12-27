97c52f63bbf45076a354ee291b7c6ff1fd8e1b07
shijinyu2011 <shi_jinyu@126.com>
2024-12-27 11:40:53 +0800

    Create start-agent.bat

---
+++ start-agent.bat
@@ -0,0 +1,3 @@
+curl -sO http://192.168.50.114:722/jnlpJars/agent.jar
+java -jar agent.jar -jnlpUrl http://192.168.50.114:722/computer/builder128/jenkins-agent.jnlp -secret d9e2281e2c9d859cc943bf4680267c4897a246b1edc2c64195ca5aa7afcb483c -workDir "C:\Users\nextvpu\Jenkins\128"
+pause
